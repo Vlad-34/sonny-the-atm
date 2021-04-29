74bf73f84023c6b23cdfca7d6b194413704bf891
Vlad <32599806+Vlad-34@users.noreply.github.com>
2021-04-29 08:55:02 +0300

    Update RAM.vhd

--- RAM.vhd
+++ RAM.vhd
@@ -1,6 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity RAM is
@@ -10,9 +9,8 @@ entity RAM is
 end RAM;
 
 architecture comportamental of RAM is	  
-
-type mem is array(0 to 11) of std_logic_vector(0 to 15);
-signal memorie: mem := (
+type memory is array(0 to 11) of std_logic_vector(0 to 15);
+signal memorie: memory := (
 	"1001111010010011", "1011101000100010", "0110010100001010", "1111010000011100", -- carduri
 	"1001011101010111", "1101000011001100", "0011101010001101", "1110001010011100", -- PIN-uri
 	"0000101101001101", "0010101001001110", "0011011010000100", "0011100110100111"  -- sume
