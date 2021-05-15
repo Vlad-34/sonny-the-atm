9ac3195505f90d55e78b1e019d473a5c5c7c9fb2
Vlad <32599806+Vlad-34@users.noreply.github.com>
2021-05-15 15:26:49 +0300

    Update sonny.vhd

--- sonny.vhd
+++ sonny.vhd
@@ -1,66 +1,85 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;  
+use ieee.std_logic_arith.all;
 library work;
 
 use work.pachet_stari.all;
 
-entity sonny is
-	port(clk, rst: in std_logic;
-	input: in std_logic_vector(15 downto 0); 
-	catod: out std_logic_vector(0 to 6);
-	anod: out std_logic_vector(0 to 3);
-	chitanta: out std_logic);
-end sonny;
-
-architecture structural of sonny is
-
-component organigrama is
+entity organigrama is
 	port(clk, rst: in std_logic;
 	card_valid, PIN_valid, fonduri_suficiente, chitanta, revenire_operatiuni: in std_logic;
 	operatiune:	in bit_vector(0 to 1);
 	LED_chitanta: out std_logic;
 	stare_curenta: inout type_states);
-end component;
+end organigrama;
+	
+architecture comportamental of organigrama is
 
-component afisor is
-	port(valoare: in std_logic_vector(0 to 15);
-		clk: in std_logic;
-		clr: in std_logic;
-		catod: out std_logic_vector(0 to 6);
-		anod: out std_logic_vector(0 to 3));
-end component;
+	signal stare, stare_urmatoare: type_states;
 
-component RAM is
-	port(clk: in std_logic;
-	mode: in std_logic_vector(1 downto 0); -- 00 - read carduri; 01 - read PIN-uri; 10 - read sume; 11 - write PIN-uri;
-	valoare: inout std_logic_vector(0 to 15); -- valori pe 16 biti
-	adresa: in std_logic_vector(0 to 3); -- 4 seturi de 3 campuri care se scriu pe 4 biti
-	output: out std_logic);
-end component;
+begin								 
+	CLS: process(clk, rst, card_valid, PIN_valid, fonduri_suficiente, chitanta, revenire_operatiuni, operatiune, stare_curenta)
+	begin
+		if rst = '1' then
+			stare <= introducere_card;
+		elsif clk'event and clk = '1' then
+			stare <= stare_urmatoare;
+		end if;	  
+	end process;	  	 
+	
+	CLC: process(stare, card_valid, PIN_valid, operatiune, chitanta, revenire_operatiuni)
+	begin
+		case stare is
+			when introducere_card =>
+				if card_valid = '1' then stare_urmatoare <= introducere_PIN;
+					else stare_urmatoare <= introducere_card;
+					end if;	 
+					
+			when introducere_PIN =>
+				if PIN_valid = '1' then stare_urmatoare <= alegere_operatiune;
+				else stare_urmatoare <= introducere_PIN; 
+				end if;
+					
+			when alegere_operatiune =>			
+				if operatiune = "00" then stare_urmatoare <= interogare_sold; -- ramura 1
+				elsif operatiune = "01" then stare_urmatoare <= introducere_PIN_nou; -- ramura 2
+				elsif operatiune = "10" then stare_urmatoare <= introducere_suma_depunere; -- ramura 3
+				elsif operatiune = "11" then stare_urmatoare <= introducere_suma_retragere; -- ramura 4
+				end if;
+			
+			when interogare_sold =>
+				stare_urmatoare <= alta_operatiune;	-- ramura 1
+			
+			when introducere_PIN_nou =>
+				stare_urmatoare <= actualizare_PIN; -- ramura 2
+			
+			when actualizare_PIN =>
+				stare_urmatoare	<= emitere_chitanta;
+			
+			when introducere_suma_depunere =>
+			stare_urmatoare <= actualizare_cont_depunere; -- ramura 3
+			
+			when actualizare_cont_depunere =>
+				stare_urmatoare <= emitere_chitanta;
+			
+			when introducere_suma_retragere =>
+				if fonduri_suficiente = '1' then stare_urmatoare <= actualizare_cont_retragere; -- ramura 4
+				else stare_urmatoare <=	introducere_suma_retragere;
+				end if;
+				
+			when actualizare_cont_retragere =>
+				stare_urmatoare <= emitere_chitanta;
+			
+			when emitere_chitanta =>
+				if chitanta = '1' then LED_chitanta <= '1';
+				end if;
+				stare_urmatoare <= alta_operatiune;
+				
+			when alta_operatiune =>
+				if revenire_operatiuni = '1' then stare_urmatoare <= alegere_operatiune;
+				else stare_urmatoare <= introducere_card;
+				end if;
 
-component sumator is
-	port(A, B: in std_logic_vector(0 to 15);
-	S: out std_logic_vector(0 to 15));
-end component;
-
-component scazator is
-	port(A, B: in std_logic_vector(0 to 15);
-	S: out std_logic_vector(0 to 15));
-end component;	
-
-component comparator_egal is
-	port(A, B: in std_logic_vector(0 to 15);
-	rez: out std_logic);
-end component; 
-
-component comparator_mic is
-	port(A, B: in std_logic_vector(0 to 15);
-	rez: out std_logic);
-end component;
-
-begin
-
-	-- your links here
-
-end structural;
+		end case;	
+	end process;
+end comportamental;
