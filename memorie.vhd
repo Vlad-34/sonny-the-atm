8cd53e33acc3e8cad43cabb7b97a2b3ecaedbd54
Vlad <32599806+Vlad-34@users.noreply.github.com>
2021-05-15 10:10:51 +0300

    Update and rename memorie to memorie.vhd

--- memorie.vhd
+++ memorie.vhd
@@ -7,7 +7,8 @@ entity RAM is
 	mode: in std_logic_vector(1 downto 0); -- 00 - read carduri; 01 - read PIN-uri; 10 - read sume; 11 - write PIN-uri;
 	valoare: inout std_logic_vector(0 to 15); -- valori pe 16 biti
 	adresa: in std_logic_vector(0 to 3); -- 4 seturi de 3 campuri care se scriu pe 4 biti
-	output: out std_logic);
+	ok: inout std_logic; -- daca operatia se efectueaza cu succes
+	id: inout integer);
 end RAM;
 
 architecture comportamental of RAM is	  
@@ -22,40 +23,29 @@ begin
 
 	process(clk, mode, valoare, adresa)	  
 	
-	variable ok: std_logic:='0'; -- returnam 1 pentru succes 
-	variable id: integer;
-	
-	begin
+	begin 
+		
+	ok <= '0'; -- returnam 1 pentru succes 
 		
 	case mode is
 		when "00" => 
-		if valoare = memorie(0) then ok:='1'; id:=0;
-		elsif valoare = memorie (1) then ok:='1'; id:=1;
-		elsif valoare = memorie (2) then ok:='1'; id:=2;
-		elsif valoare = memorie (3) then ok:='1'; id:=3;
+		if valoare = memorie(0) then ok<='1'; id<=0;
+		elsif valoare = memorie (1) then ok<='1'; id<=1;
+		elsif valoare = memorie (2) then ok<='1'; id<=2;
+		elsif valoare = memorie (3) then ok<='1'; id<=3;
 		end if;
 		
-		when "01" =>
-		if valoare = memorie(4) then ok:='1';
-		elsif valoare = memorie (5) then ok:='1';
-		elsif valoare = memorie (6) then ok:='1';
-		elsif valoare = memorie (7) then ok:='1';
+		when "01" => 
+		if valoare = memorie(id + 4) then ok <= '1';
 		end if;
 			
 		when "10" =>
-		if valoare = memorie(8) then ok:='1';
-		elsif valoare = memorie (9) then ok:='1';
-		elsif valoare = memorie (10) then ok:='1';
-		elsif valoare = memorie (11) then ok:='1'; 
+		if valoare = memorie(id + 8) then ok <= '1'; 
 		end if;
 		
 		when "11" => 
-		if id = 0 then memorie(4) <= valoare;
-		elsif id = 1 then memorie (5) <= valoare; 
-		elsif id = 2 then memorie (6) <= valoare;
-		elsif id = 3 then memorie (7) <= valoare;
-		end if;
-		ok:='1';
+		memorie(id + 4) <= valoare;
+		ok<='1';
 		
 		when others => NULL;
 	end case;
