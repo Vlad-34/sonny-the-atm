46b2ea98d8f8ed39f57d7afae4e53e8417996666
Vlad <32599806+Vlad-34@users.noreply.github.com>
2021-05-17 16:53:57 +0300

    Update memorie.vhd

--- memorie.vhd
+++ memorie.vhd
@@ -2,37 +2,36 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-entity RAM is
+entity memorie is
 	port(clk: in std_logic;
 	mode: in std_logic_vector(1 downto 0); -- 00 - read carduri; 01 - read PIN-uri; 10 - read sume; 11 - write PIN-uri;
-	valoare: inout std_logic_vector(0 to 15); -- valori pe 16 biti
-	adresa: in std_logic_vector(0 to 3); -- 4 seturi de 3 campuri care se scriu pe 4 biti
-	ok: inout std_logic; -- daca operatia se efectueaza cu succes
-	id: inout integer);
-end RAM;
+	mode2: in std_logic;
+	valoare: inout std_logic_vector(15 downto 0); -- valori pe 16 biti
+	ok: out std_logic); -- daca operatia se efectueaza cu succes);
+end memorie;
 
-architecture comportamental of RAM is	  
+architecture comportamental of memorie is	  
 type memory is array(0 to 11) of std_logic_vector(0 to 15);
 signal memorie: memory := (
 	"1001111010010011", "1011101000100010", "0110010100001010", "1111010000011100", -- carduri
 	"1001011101010111", "1101000011001100", "0011101010001101", "1110001010011100", -- PIN-uri
-	"0000101101001101", "0010101001001110", "0011011010000100", "0011100110100111"  -- sume
+	"0000101101001101", "0010101001001110", "0001000100010001", "0011100110100111"  -- sume
 	);
 
 begin
-
-	process(clk, mode, valoare, adresa, ok, id)	  
 	
+	process(clk, mode, valoare)	  
+	variable id: integer:=0;
 	begin 
 		
 	ok <= '0'; -- returnam 1 pentru succes 
 	if clk'event and clk = '1' then	
 		case mode is
-			when "00" => 
-			if valoare = memorie(0) then ok<='1'; id<=0;
-			elsif valoare = memorie (1) then ok<='1'; id<=1;
-			elsif valoare = memorie (2) then ok<='1'; id<=2;
-			elsif valoare = memorie (3) then ok<='1'; id<=3;
+			when "00" =>
+			if valoare = memorie(0) then ok<='1'; id:=0;
+			elsif valoare = memorie (1) then ok<='1'; id:=1;
+			elsif valoare = memorie (2) then ok<='1'; id:=2;
+			elsif valoare = memorie (3) then ok<='1'; id:=3;
 			end if;
 			
 			when "01" => 
@@ -43,10 +42,13 @@ begin
 			if valoare = memorie(id + 8) then ok <= '1'; 
 			end if;
 			
-			when "11" => 
-			memorie(id + 4) <= valoare;
-			ok<='1';
-			
+			when "11" => 	
+				if mode2 = '0' then
+					memorie(id + 4) <= valoare;
+				else
+					memorie(id + 8) <= valoare;
+				end if;	   
+				ok<='1';
 			when others => NULL;
 		end case;
 	end if;
